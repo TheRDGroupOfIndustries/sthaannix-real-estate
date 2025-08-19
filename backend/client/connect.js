627bf0c124367d714a7158ac6b879473d0604c6d
muskanrajputsingh <123744013+muskanrajputsingh@users.noreply.github.com>
2025-08-19 12:09:26 +0530

    Delete backend directory

--- backend/client/connect.js
+++
@@ -1,8 +0,0 @@
-// client/connect.ts
-import { PrismaClient } from "@prisma/client";
-
-const db = new PrismaClient();
-
-db.$connect().then(() => console.log("Database connected")).catch(console.error);
-
-export default db;

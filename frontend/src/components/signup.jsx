4217a88104df62ec83add2796fef627ad5b52d36
muskanrajputsingh <123744013+muskanrajputsingh@users.noreply.github.com>
2025-08-28 14:59:01 +0530

    Update signup.jsx

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -95,7 +95,7 @@ const Signup = () => {
     phone: "",
     email: "",
     password: "",
-    role: "user"
+    role: "buyer"
   });
   const [otp, setOtp] = useState("");
   const [otpSent, setOtpSent] = useState(false);
